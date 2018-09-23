76cc4f73676899fb5cb49bd760603bb6b7752207
Sam Wang <samwang2002@sams-macbook.local>
2018-09-22 23:38:22 -0500

    Initial Commit

---
+++ Bus TrackerTests/Bus_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bus_TrackerTests.swift
+//  Bus TrackerTests
+//
+//  Created by Sam Wang  on 9/22/18.
+//  Copyright © 2018 Sam Wang. All rights reserved.
+//
+
+import XCTest
+@testable import Bus_Tracker
+
+class Bus_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
